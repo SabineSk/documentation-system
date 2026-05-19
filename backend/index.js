c60acf595b589c78e133bf61c6da0e70b5f060fb
SabineSk <sabine.skeranska@gmail.com>
2026-05-19 15:50:33 +0300

    finding user, comparing password added

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,8 @@
-//Backend fails
-//palaiž Express serveri
-//pieslēdz middleware
-//pieslēdz routes
-//pieslēdz MongoDB
-//sāk klausīties portu
-
-
 require('dotenv').config();
 const bcrypt = require('bcrypt');
 const createUserIfNotExists = require("./seeds/users");
 
+const User = require("./models/user.model");
 const express = require('express')
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -22,18 +15,34 @@ const port = 3000
 app.use(cors());
 app.use(express.json());
 
-app.post('/api/login', (req, res) => {
+app.post('/api/login', async (req, res) => {
   const { username, password } = req.body;
-  //atrast lietotāju datubāzē pēc username
-  //ja lietotājs nav atrasts, atgriezt kļūdu
-  //ja lietotājs ir atrasts, salīdzināt paroli ar datubāzē saglabāto hash paroli
-  //ja parole nesakrīt, atgriezt kļūdu
-  //ja parole sakrīt, atgriezt veiksmīgu atbildi ar lietotāja informāciju (piemēram, username un role)
-  console.log(username, password);
+//the login route waits for User.findOne(...) and returns a response only once, inside the try/catch
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.json({ message: "User not found" });
+    }
 
-  console.log(req.body.username);
+    const passwordMatch = await bcrypt.compare(password, user.password);
 
-  res.json({ message: "Login successful", username });
+    if (!passwordMatch) {
+      return res.json({ message: "Invalid password" });
+    }
+    //TODO: Labot,jo šobrīd atriež Login successful arī ja user not found
+    if (user && passwordMatch) {
+      // Šeit varētu ģenerēt un atgriezt JWT tokenu, ja nepieciešams
+      
+      console.log(username, password);
+      console.log(req.body.username);
+
+      return res.json({ message: "Login successful, username and password correct", username: user.username, role: user.role });
+  }
+    
+  } catch (error) {
+    console.error(error);
+    return res.json({ message: "Error occurred while logging in" });
+  }
 });
 
 app.get('/api/documents', (req, res) => {
